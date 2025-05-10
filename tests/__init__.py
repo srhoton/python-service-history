c95220dfb5702dcd5765dfde9c120bb8ee945ec5
Steve Rhoton <steve.rhoton@fullbay.com>
2025-05-09 20:23:09 -0400

    ruff fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """Service history lambda function test package.
 
-This package contains tests for the AWS Lambda functions 
+This package contains tests for the AWS Lambda functions
 that handle service history logging and retrieval.
 """
